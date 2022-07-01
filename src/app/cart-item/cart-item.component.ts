95cfe40235577a3f13fcd0ccede5341c6c35d7bd
Bhashitha Arambepola <barambepola@gmail.com>
2022-07-01 14:35:04 +0530

    finalize the rating

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -10,7 +10,7 @@ export class CartItemComponent implements OnInit {
 item = new Item("1001",
   "https://m.media-amazon.com/images/I/41-aexp44tL._AC_UL320_.jpg",
   "Apple AirPods (2nd Generation)",
-  4,
+  2,
   299,
   5)
   constructor() { }

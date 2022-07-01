799c417a2a17e0ad540c5da7871d39c58df47b60
Bhashitha Arambepola <barambepola@gmail.com>
2022-07-01 18:53:19 +0530

    add dummy data set

--- src/app/dummy-data.ts
+++ src/app/dummy-data.ts
@@ -1,2 +1,31 @@
-export class DummyData {
-}
+import {Item} from "./dto/item";
+
+const DUMMY_DATA: Array<Item> = [
+    new Item("1001",
+      "https://m.media-amazon.com/images/I/41-aexp44tL._AC_UL320_.jpg",
+      "Apple EarPods with Lightning Connector - White",
+      2,
+      299,
+      5),
+    new Item("1002",
+      "https://m.media-amazon.com/images/I/7120GgUKj3L._AC_UL320_.jpg",
+      "Apple AirPods (2nd Generation)",
+      4,
+      450,
+      25),
+    new Item("1003",
+      "https://m.media-amazon.com/images/I/31mmFtpMqOL._AC_UL320_.jpg",
+      "Apple Lightning to 3.5 mm Headphone Jack Adapter",
+      4,
+      50,
+      20),
+    new Item("1004",
+      "https://m.media-amazon.com/images/I/71Z09LgdOPL._AC_UL320_.jpg",
+      "TOZO T6 True Wireless Earbuds Bluetooth Headphones Touch Control with Wireless Charging Case IPX8 Waterproof Stereo Earphones in-Ear Built-in Mic Headset Premium Deep Bass for Sport Black",
+      4,
+      350,
+      15)
+
+
+  ]
+;

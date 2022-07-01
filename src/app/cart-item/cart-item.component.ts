66a9a72dee50298fffffbf7dcc517522fcf28df8
Bhashitha Arambepola <barambepola@gmail.com>
2022-07-01 13:34:04 +0530

    update cart-item template

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Item} from "../dto/item";
 
 @Component({
   selector: 'app-cart-item',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-item.component.scss']
 })
 export class CartItemComponent implements OnInit {
-
+item = new Item("1001",
+  "https://m.media-amazon.com/images/I/41-aexp44tL._AC_UL320_.jpg",
+  "Apple AirPods (2nd Generation)",
+  4,
+  299,
+  5)
   constructor() { }
 
   ngOnInit(): void {
--- src/app/dto/item.ts
+++ src/app/dto/item.ts
@@ -3,7 +3,7 @@ export class Item {
   constructor(public  code: string,
               public  image: string,
               public  description: string,
-              public rating: 1|2|3|4|5,
+              public rating: 1 | 2 | 3 | 4 | 5,
               public price: number,
               public  qty:  number) {
   }
